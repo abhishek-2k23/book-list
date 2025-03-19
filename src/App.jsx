b779cfd5f6ec4d73ae733661c0f39bb34ef31354
abhishek-2k23 <kumarabhishek00090@gmail.com>
2025-03-19 23:36:37 +0530

    navbar and bookcontainer with bg-style

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import BooksContainer from './components/BooksContainer';
 
 function App() {
   return (
-    <div className='bg-red-500 mt-5 '>App</div>
+    <div className='text-gray-200 bg-radial from-slate-700  via-gray-950  to-slate-800 w-full min-h-screen flex flex-col items-center pb-5'>
+      <Navbar />
+      <BooksContainer />
+    </div>
   )
 }
 
