98a37a0d7cf9c224cbc891781a965bbdc16078ed
abhishek-2k23 <kumarabhishek00090@gmail.com>
2025-03-20 01:51:30 +0530

    react-hot-toast

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AppContextProvider } from './contextApi/AppContext.jsx'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <AppContextProvider><App /></AppContextProvider> 
+   <AppContextProvider><App /><Toaster/></AppContextProvider> 
   </StrictMode>,
 )
